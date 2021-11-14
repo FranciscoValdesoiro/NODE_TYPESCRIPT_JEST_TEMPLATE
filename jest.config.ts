e802b4de9ab62b772d0236e3e674632c1713ffd0
FranciscoValdesoiro <franciscovaldesoirogil@gmail.com>
2021-11-14 10:36:19 +0100

    main commit

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  preset: "ts-jest",
+    testEnvironment: "node",
+    transform: {
+      "node_modules/variables/.+\\.(j|t)sx?$": "ts-jest"
+    },
+    transformIgnorePatterns: [
+      "node_modules/(?!variables/.*)"
+    ]
+};
